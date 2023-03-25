7b490de16f8fed5d65fdfbde3dc4922a140e5b35
Daniel Nikbakht Tehrani <e12119896@student.tuwien.ac.at>
2023-03-25 03:09:57 +0100

    Update header.component.ts

--- frontend/xmrscan-ng/src/app/component/header/header.component.ts
+++ frontend/xmrscan-ng/src/app/component/header/header.component.ts
@@ -8,31 +8,32 @@ import { Component, HostListener } from '@angular/core';
 export class HeaderComponent{
 
   visible = true
+  delay = 40
 
   addScroll() {
-    setTimeout(function(){document.getElementById('info1')?.classList.add('scrolled');}, 0);
-    setTimeout(function(){document.getElementById('info2')?.classList.add('scrolled');}, 30);
-    setTimeout(function(){document.getElementById('info3')?.classList.add('scrolled');}, 60);
-    setTimeout(function(){document.getElementById('info4')?.classList.add('scrolled');}, 90);
-    setTimeout(function(){document.getElementById('info5')?.classList.add('scrolled');}, 120);
-    setTimeout(function(){document.getElementById('info6')?.classList.add('scrolled');}, 150);
-    setTimeout(function(){document.getElementById('info7')?.classList.add('scrolled');}, 180);
-    setTimeout(function(){document.getElementById('info8')?.classList.add('scrolled');}, 210);
-    setTimeout(function(){document.getElementById('info9')?.classList.add('scrolled');}, 240);
-    setTimeout(function(){document.getElementById('info10')?.classList.add('scrolled');}, 270);
+    setTimeout(function(){document.getElementById('info1')?.classList.add('scrolled');}, 0*this.delay);
+    setTimeout(function(){document.getElementById('info2')?.classList.add('scrolled');}, 1*this.delay);
+    setTimeout(function(){document.getElementById('info3')?.classList.add('scrolled');}, 2*this.delay);
+    setTimeout(function(){document.getElementById('info4')?.classList.add('scrolled');}, 3*this.delay);
+    setTimeout(function(){document.getElementById('info5')?.classList.add('scrolled');}, 4*this.delay);
+    setTimeout(function(){document.getElementById('info6')?.classList.add('scrolled');}, 5*this.delay);
+    setTimeout(function(){document.getElementById('info7')?.classList.add('scrolled');}, 6*this.delay);
+    setTimeout(function(){document.getElementById('info8')?.classList.add('scrolled');}, 7*this.delay);
+    setTimeout(function(){document.getElementById('info9')?.classList.add('scrolled');}, 8*this.delay);
+    setTimeout(function(){document.getElementById('info10')?.classList.add('scrolled');}, 9*this.delay);
   }
 
   removeScroll() {
-    setTimeout(function(){document.getElementById('info1')?.classList.remove('scrolled');}, 0);
-    setTimeout(function(){document.getElementById('info2')?.classList.remove('scrolled');}, 30);
-    setTimeout(function(){document.getElementById('info3')?.classList.remove('scrolled');}, 60);
-    setTimeout(function(){document.getElementById('info4')?.classList.remove('scrolled');}, 90);
-    setTimeout(function(){document.getElementById('info5')?.classList.remove('scrolled');}, 120);
-    setTimeout(function(){document.getElementById('info6')?.classList.remove('scrolled');}, 150);
-    setTimeout(function(){document.getElementById('info7')?.classList.remove('scrolled');}, 180);
-    setTimeout(function(){document.getElementById('info8')?.classList.remove('scrolled');}, 210);
-    setTimeout(function(){document.getElementById('info9')?.classList.remove('scrolled');}, 240);
-    setTimeout(function(){document.getElementById('info10')?.classList.remove('scrolled');}, 270);
+    setTimeout(function(){document.getElementById('info1')?.classList.remove('scrolled');}, 0*this.delay);
+    setTimeout(function(){document.getElementById('info2')?.classList.remove('scrolled');}, 1*this.delay);
+    setTimeout(function(){document.getElementById('info3')?.classList.remove('scrolled');}, 2*this.delay);
+    setTimeout(function(){document.getElementById('info4')?.classList.remove('scrolled');}, 3*this.delay);
+    setTimeout(function(){document.getElementById('info5')?.classList.remove('scrolled');}, 4*this.delay);
+    setTimeout(function(){document.getElementById('info6')?.classList.remove('scrolled');}, 5*this.delay);
+    setTimeout(function(){document.getElementById('info7')?.classList.remove('scrolled');}, 6*this.delay);
+    setTimeout(function(){document.getElementById('info8')?.classList.remove('scrolled');}, 7*this.delay);
+    setTimeout(function(){document.getElementById('info9')?.classList.remove('scrolled');}, 8*this.delay);
+    setTimeout(function(){document.getElementById('info10')?.classList.remove('scrolled');}, 9*this.delay);
   }
 
   @HostListener('window:scroll', ['$event']) onScrollEvent($event: any){
